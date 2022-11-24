f312f36752d615bdfd1380b887328ac456a6c9db
vantanto <56611021+vantanto@users.noreply.github.com>
2022-11-25 05:47:45 +0700

    edit: nav + footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,17 +3,17 @@
         <div class="row text-center align-items-center flex-row-reverse">
             <div class="col-lg-auto ms-lg-auto">
                 <ul class="list-inline list-inline-dots mb-0">
-                    <li class="list-inline-item"><a href="./docs/index.html" class="link-secondary">Documentation</a></li>
-                    <li class="list-inline-item"><a href="./license.html" class="link-secondary">License</a></li>
-                    <li class="list-inline-item"><a href="https://github.com/tabler/tabler" target="_blank" class="link-secondary" rel="noopener">Source code</a></li>
+                    <li class="list-inline-item"><a href="https://github.com/vantanto/vwallet" target="_blank" class="link-secondary" rel="noopener">Source code</a></li>
                     <li class="list-inline-item">
-                        <a href="https://github.com/sponsors/codecalm" target="_blank" class="link-secondary" rel="noopener">
-                            <!-- Download SVG icon from http://tabler-icons.io/i/heart -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon text-pink icon-filled icon-inline" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
-                            </svg>
-                            Sponsor
+                        <span>Build with</span>
+                        <!-- Download SVG icon from http://tabler-icons.io/i/heart -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon text-pink icon-filled icon-inline" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+                        </svg>
+                        <span>by</span>
+                        <a href="https://github.com/vantanto" target="_blank" class="link-secondary" rel="noopener">
+                            vantanto
                         </a>
                     </li>
                 </ul>
@@ -22,14 +22,14 @@
                 <ul class="list-inline list-inline-dots mb-0">
                     <li class="list-inline-item">
                         Copyright &copy; 2022
-                        <a href="." class="link-secondary">Tabler</a>.
+                        <a href="." class="link-secondary">vwallet</a>.
                         All rights reserved.
                     </li>
-                    <li class="list-inline-item">
+                    {{-- <li class="list-inline-item">
                         <a href="./changelog.html" class="link-secondary" rel="noopener">
                             v1.0.0-beta5
                         </a>
-                    </li>
+                    </li> --}}
                 </ul>
             </div>
         </div>
